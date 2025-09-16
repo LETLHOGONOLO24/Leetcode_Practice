672b2cee618d8ab0786c66fe8183912a37333fc3
LETLHOGONOLO24 <Pitroschauke24@gmail.com>
2025-09-16 16:39:18 +0200

    Added comments to the code

--- Easy/MergeTwoLists.py
+++ Easy/MergeTwoLists.py
@@ -25,7 +25,26 @@ The number of nodes in both lists is in the range [0, 50].
 Both list1 and list2 are sorted in non-decreasing order.
 
 
+STEPS
 
+1 - Create a function that takes in 2 lists as its parameters
+2 - Inside the function, create a 3rd list that will be the result of
+    2 added lists
+
+3 - swapped = True because for the while loop to keep looping, the
+    condition must be true
+4 - for swapped to be false, we are saying this is the last time we 
+    loop because there's no need for swaps
+
+5 - The for loop loops through the length of list3 minus 1 because we
+    won't need to check the last element
+6 - The if statement checks whether the current iteration is > the next
+    iteration and if the statement is true, we're going to swap the
+    current with the next
+
+7 - Set swapped to True because we must check other elements in the list
+    whether they need to be swapped or not
+8 - Return list3
 
 
 """
@@ -51,6 +70,6 @@ class Solution:
 sol = Solution()
 
 list1 = [1,2,3]
-list2 = [1,2,4]
+list2 = [1,3,4]
 
 print(sol.listNode(list1,list2))

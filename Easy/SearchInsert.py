c09219b0a4a421218d94e60c29e4aa3e29f029ad
LETLHOGONOLO24 <Pitroschauke24@gmail.com>
2025-09-20 14:02:23 +0200

    Adding code to the file

--- Easy/SearchInsert.py
+++ Easy/SearchInsert.py
@@ -34,3 +34,31 @@ STEPS
 
 
 """
+
+class Solution:
+    def searchInsert(self, nums, target):
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] < target:
+                left = mid + 1
+
+            else:
+                if nums[mid] > target:
+                    right = mid - 1
+
+        return left
+    
+sol = Solution()
+
+nums = [1,3,5,6]
+target = 7
+
+print(sol.searchInsert(nums, target))

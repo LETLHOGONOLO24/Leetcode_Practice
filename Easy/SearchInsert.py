fa10ffef7c1d88f61a2bbfdb1d38ec5682ee7e93
LETLHOGONOLO24 <Pitroschauke24@gmail.com>
2025-09-20 14:40:23 +0200

    Adding the structure of how the code was written

--- Easy/SearchInsert.py
+++ Easy/SearchInsert.py
@@ -30,7 +30,43 @@ nums contains distinct values sorted in ascending order.
 
 STEPS
 
+Start with the entire sorted array
+Find the middle element
 
+Compare middle element with target:
+If equal → Found it!
+If target is smaller → Search left half
+
+If target is larger → Search right half
+Repeat until found or array is empty
+
+
+1 -  This problem required us to use binary search because of 0(log n)
+    runtime complexity
+2 - We declare a function searchInsert that takes a nums array and int
+    length as parameters
+
+3 - Declare left = 0 because we start at the 1st element. declare right
+    to len(nums) - 1 because the valid indexes are from 0 to 3. Meaning
+    nums[4] doesn't exist and it will be out of bounds
+    
+4 - Set the while loop left <= right because binary search shrinks the
+    search space by adjusting left and right, and this algorithm is
+    used for arrays of ascending order
+
+5 - Declare mid = (left + right) // 2 because we want the middle index.
+    Binary search works by checking the middle element, then deciding
+    whether to search the left half or the right half.
+
+6 - If nums[mid] == target: we found it → return mid.
+    If nums[mid] < target: the target must be to the right of mid.
+    So, move the left boundary up: left = mid + 1.
+
+    If nums[mid] > target: the target must be to the left of mid.
+    So, move the right boundary down: right = mid - 1.
+
+7 - If the target wasn’t found, left will stop at the position
+    where the target should be inserted.
 
 
 """
